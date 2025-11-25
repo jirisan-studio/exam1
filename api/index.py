355e61385f68cfca1cd12aca497f30e43f38304e
jirisan-studio <nature.kr@gmail.com>
2025-11-25 21:38:56 +0900

    수정

--- api/index.py
+++ api/index.py
@@ -3,20 +3,16 @@ from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app) # 모든 곳에서 접속 허용
+CORS(app)
 
 @app.route('/api/proxy')
 def proxy():
     url = request.args.get('url')
     if not url:
         return "URL is required", 400
-    
     try:
-        # 국가유산청 서버인 척 헤더 설정
         headers = {'User-Agent': 'Mozilla/5.0'}
-        # 데이터 요청
         resp = requests.get(url, headers=headers)
-        # 받은 XML 데이터를 그대로 반환
         return Response(resp.content, mimetype='text/xml')
     except Exception as e:
-        return str(e), 500  
+        return str(e), 500
